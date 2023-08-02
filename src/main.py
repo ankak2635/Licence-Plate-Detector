55bd77e4b3c5136f355e98323e0261a7914de4f0
ankak2635 <ank.ak2635@gmail.com>
2023-08-02 10:25:01 +0530

    Adds sort submodule

---
+++ src/main.py
@@ -0,0 +1,30 @@
+from ultralytics import YOLO
+import cv2
+
+# load models
+vehicle_detector = YOLO('yolov8n.pt')
+plate_detector =  YOLO('./models/license_plate_detector.pt')
+
+# load video
+
+# read frames
+
+
+    # detect vehicles
+
+
+    # track vehicles
+
+    # detect licence plate
+
+
+    # assign licence plate to car
+
+
+    # crop licence plate
+
+
+    # process licence plate
+
+
+    # read licence plate
